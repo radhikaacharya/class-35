022b3d453d2986168a846814f95ee44b77321579
radhikaacharya <64483462+radhikaacharya@users.noreply.github.com>
2020-10-16 19:00:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+
+var database,form,player,game,playerCount,canvas
+var gameState=0
+function setup(){
+  canvas =   createCanvas(400,400);
+    
+    database = firebase.database();
+
+}
+
+function draw(){
+   
+}
